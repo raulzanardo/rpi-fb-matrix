19583c2dbc06a769e2b20a61df17019cf182c179
Raul Zanardo <raulzanardo@gmail.com>
2025-07-28 22:50:07 -0300

    test script

---
+++ test.sh
@@ -0,0 +1,17 @@
+sudo ./display-test matrix.cfg 
+--led-pwm-dither-bits=0 \
+--led-gpio-mapping=regular \
+--led-brightness=60 \
+--led-rows=64 \
+--led-cols=64 \
+--led-chain=3 \
+--led-parallel=2 \
+--led-slowdown-gpio=4 \
+--led-show-refresh \
+--led-scan-mode=0 \
+--led-limit-refresh=90 \
+--led-pwm-bits=11 \
+--led-show-refresh \
+--led-panel-type=FM6126A
+
+
